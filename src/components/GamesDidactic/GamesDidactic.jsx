32786b1f79fe877d03f02c4ced3d9ed3b20277c7
LucasCorbalan <lucas.corbalan.23@gmail.com>
2024-02-22 19:58:31 -0300

    horizontal offset error correction -- test5

--- src/components/GamesDidactic/GamesDidactic.jsx
+++ src/components/GamesDidactic/GamesDidactic.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const GamesDidactic = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white">
-      <div className="max-w-screen-md mx-auto py-12 ">
+      <div className="mx-auto py-10 ">
         <h1
           data-aos="zoom-out"
           data-aos-duration="300"
-          className="text-base sm:text-sm font-bold text-center pt-12"
+          className="text-base sm:text-sm font-bold text-center pt-8"
         >
           JUEGOS DIDACTICOS
         </h1>
