194149f58eaf70e640486b5fc748d6f5fdb05679
LucasCorbalan <lucas.corbalan.23@gmail.com>
2024-02-22 20:21:39 -0300

    horizontal offset error correction

--- src/components/GamesDidactic/GamesDidactic.jsx
+++ src/components/GamesDidactic/GamesDidactic.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const GamesDidactic = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white">
-      <div className="mx-auto py-10 ">
+      <div className="px-4 py-12 sm:px-6 lg:px-8">
         <h1
           data-aos="zoom-out"
           data-aos-duration="300"
-          className="text-base sm:text-sm font-bold text-center pt-8"
+          className="text-2xl sm:text-6xl lg:text-2xl font-bold text-center pt-12"
         >
           JUEGOS DIDACTICOS
         </h1>
--- src/components/MangaAndComics/MangaAndComics.jsx
+++ src/components/MangaAndComics/MangaAndComics.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const MangaAndComics = () => {
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white">
-      <div className="max-w-screen-lg mx-auto px-4 py-12 sm:px-6 lg:px-8">
+      <div className="px-4 py-12 sm:px-6 lg:px-8">
         <h1
           data-aos="zoom-out"
           data-aos-duration="300"
-          className="text-lg sm:text-sm lg:text-lg font-bold text-center pt-12"
+          className="text-2xl sm:text-6xl lg:text-2xl font-bold text-center pt-12"
         >
           MANGA Y COMICS
         </h1>
