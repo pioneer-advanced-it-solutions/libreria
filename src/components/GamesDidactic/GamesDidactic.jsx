755156dfd726ded71934c9cb67168b5103f046b7
LucasCorbalan <lucas.corbalan.23@gmail.com>
2024-02-22 19:55:01 -0300

    horizontal offset error correction -- test4

--- src/components/GamesDidactic/GamesDidactic.jsx
+++ src/components/GamesDidactic/GamesDidactic.jsx
@@ -2,18 +2,17 @@ import React from "react";
 
 const GamesDidactic = () => {
   return (
-    // <div className="bg-white dark:bg-gray-900 dark:text-white">
-    //   <div className="max-w-screen-md mx-auto px-4 py-12 sm:px-6 lg:px-8 ">
-    //     <h1
-    //       data-aos="zoom-out"
-    //       data-aos-duration="300"
-    //       className="text-lg sm:text-sm lg:text-lg font-bold text-center pt-12"
-    //     >
-    //       JUEGOS DIDACTICOS
-    //     </h1>
-    //   </div>
-    // </div>
-    <div></div>
+    <div className="bg-white dark:bg-gray-900 dark:text-white">
+      <div className="max-w-screen-md mx-auto py-12 ">
+        <h1
+          data-aos="zoom-out"
+          data-aos-duration="300"
+          className="text-base sm:text-sm font-bold text-center pt-12"
+        >
+          JUEGOS DIDACTICOS
+        </h1>
+      </div>
+    </div>
   );
 };
 
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -22,11 +22,11 @@ const Products = () => {
 
   return (
     <div className="bg-white dark:bg-gray-900 dark:text-white">
-      <div className="max-w-screen-md mx-auto px-4 py-12 sm:px-6 lg:px-8 ">
+      <div className="max-w-screen-md mx-auto py-12 ">
         <h1
           data-aos="zoom-out"
           data-aos-duration="300"
-          className="text-lg sm:text-sm lg:text-lg font-bold text-center pt-12"
+          className="text-lg sm:text-sm font-bold text-center pt-12"
         >
           TODOS LOS LIBROS
         </h1>
